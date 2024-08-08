a276beba1bbd5d276bb06a91eca6d1f7585361be
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-08-08 11:43:36 +0300

    state-events-lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const[inCart, setCart] = useState(false);
+
+  function handleCart(){
+    setCart((prevInCart) => !prevInCart)
+  }
+
+
+
   return (
-    <li className="">
+    <li className={inCart? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}> { inCart? "Remove from Cart": "Add to Cart"}</button>
     </li>
   );
 }
